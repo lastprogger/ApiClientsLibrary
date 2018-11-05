3fa59e36e6acecf148a9d993eac906be3e7f1bcc
karavaev stanislav <stanislav.karavaev@factorymarket.com>
2018-11-05 22:14:27 +0300

    Fixes

--- readme.md
+++ readme.md
@@ -1,10 +1,14 @@
 ### ApiClientsLibrary
 
 1. `php artisan vendor:publish` and choice InternalApiServiceProvider
-2. Add to .env 
+2. Add service provider `InternalApi\InternalApiServiceProvider::class` to config/app.php
+
+3. Add to .env 
 
 INTERNAL_API_TOKEN=
 
 HEADER_INTERNAL_API_TOKEN=
 
 SERVICE_PHONE_NUMBER_HOST=
+
+SERVICE_PHONE_NUMBER_API_VERSION=v1
--- src/config/api.php
+++ src/config/api.php
@@ -4,13 +4,13 @@ return [
     'internal_api_token'        => env('INTERNAL_API_TOKEN'),
     'header_internal_api_token' => env('HEADER_INTERNAL_API_TOKEN'),
     'service_phone_number'      => [
-        'host'     => env('SERVICE_PHONE_NUMBER_HOST'),
+        'host'     => env('SERVICE_PHONE_NUMBER_HOST' , 'asd'),
         'endpoint' => [
             'did' => [
-                'get'           => env('SERVICE_PHONE_NUMBER_HOST') . '/did/{id}',
-                'create'        => env('SERVICE_PHONE_NUMBER_HOST') . '/did',
-                'list'          => env('SERVICE_PHONE_NUMBER_HOST') . '/did',
-                'change_status' => env('SERVICE_PHONE_NUMBER_HOST') . '/did/{id}/change-status',
+                'get'           => env('SERVICE_PHONE_NUMBER_HOST') . '/api/'. env('SERVICE_PHONE_NUMBER_API_VERSION') .'/did/{id}',
+                'create'        => env('SERVICE_PHONE_NUMBER_HOST') . '/api/'. env('SERVICE_PHONE_NUMBER_API_VERSION') . '/did',
+                'list'          => env('SERVICE_PHONE_NUMBER_HOST') . '/api/'. env('SERVICE_PHONE_NUMBER_API_VERSION') . '/did',
+                'change_status' => env('SERVICE_PHONE_NUMBER_HOST') . '/api/'. env('SERVICE_PHONE_NUMBER_API_VERSION') . '/did/{id}/change-status',
             ],
         ],
     ],
